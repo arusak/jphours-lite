b768d6fc29dee30b2b52cad32b731e2d4463a992
Anton Rusak <4231915+arusak@users.noreply.github.com>
2026-08-23 17:00:38 +0200

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import yaml from '@rollup/plugin-yaml'
 import { VitePWA } from 'vite-plugin-pwa'
-// @ts-ignore
 import { readableCssModules } from 'vite-plugin-readable-css-modules'
 
 export default defineConfig({
